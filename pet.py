84a46d0af4191154081e2cfb279a608bc169a604
AidenCM1234 <aidenm124@nycstudents.net>
2025-11-06 09:25:49 -0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -1,4 +1,4 @@
-class Calculator():
+"""class Calculator():
     def add(x, y):
         print(x + y)
         return x + y
@@ -38,16 +38,58 @@ class BankAccount:
         print(f"{self.owner} has ${self.__balance}")
 
 Bob =BankAccount("Bob", 10000)
-Bob.show_balance()
+Bob.show_balance()"""
 
 class Pet:
-    def __init__(self, name, happyness):
+    def __init__(self, name, happyness, life):
         self.name = name
         self.happyness = happyness
+        self.life= life
 
     def play(self,game):
         self.happyness += game
         print (f"{self.name} is playing {game}")
+        print(f"{self.name}'s happyness is now {self.happyness}")
     
-    def check(self,name):
-        print(self.name)
+    def discard(self):
+        self.life = False
+        print(f"{self.name}'s happyness was {self.happyness}")
+
+    def check(self):
+        print(f"{self.name}'s happyness is currently {self.happyness}")
+
+
+x= input("What would you like to name your pet? ")
+x =Pet(f"{x}", 0, True)
+x.check()
+#while self.life == True:
+
+life=True
+
+while life:
+    isthing = False
+    while not isitem:
+        choice = input("Please choose one item to purchase: ")
+        
+        for item in store:
+            if choice == item["name"]: 
+                print(f"You have purchased one {item['name']} for ${item['price']}")
+                cart.append(f"{item['name']} - ${item['price']}")
+                total+=item['price']
+                isitem = True
+        
+        if not isitem:  
+            print("Try again")
+    a=False
+    while not a:
+        again = input("Would you like to purchase more items? (Y/N): ")
+        if again == 'N':
+            done = True
+            a=True 
+        elif again == 'Y':
+            a=True
+        elif again != 'N' or 'Y':
+            print("Try again")
+
+
+    
