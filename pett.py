8999dab9b92bf9ff7fb4a499c79d6b8928646696
AidenCM1234 <aidenm124@nycstudents.net>
2025-11-13 09:25:46 -0500

    Update pett.py

--- pett.py
+++ pett.py
@@ -8,12 +8,14 @@ class Pet:
   
    def discard(self):
        self.life = False
-       print(f"{self.name}'s dead now \n"
-             f"{self.name}'s happyness was {self.happyness} \n")
+       print(f"\n{self.name}'s dead now \n"
+             f"{self.name}'s happyness was {self.happyness}")
        if self.hunger >20:
-           (f"{self.name}'s starved")
-       if self.hunger <0:
-           (f"{self.name} died from obeisty")
+           print(f"{self.name}'s starved")
+       elif self.hunger <= -5:
+           print(f"{self.name} died from obeisty")
+       elif self.happyness < -5:
+            print(f"{self.name} died from depersion")
   
 
    def play(self,value, game):
@@ -32,13 +34,13 @@ class Pet:
        print(f"{self.name}'s happyness is currently {self.happyness}")
        print(f"{self.name}'s hunger is currently {self.hunger}")
 
-
+alleric=[]
 import random
 
 x= input("What would you like to name your pet? ")
 x =Pet(f"{x}", 0, True, 10)
 x.check()
-
+#inplay =False
 while x.life:
    isitem = False
    while not isitem:
@@ -49,27 +51,35 @@ while x.life:
        "1: Check Stats \n"
        "2: Discard Pet \n"
        "3: Feed \n").lower()
-       if r <= 2 and x.life:
+       if r <= 2 and x.life:# and not inplay:
            x.hunger =+ r
            print(f"{x.name} randomly got hunger increased by {r}")
            if x.hunger < 0:
                 x.discard()
                 break
-       if a <= 2 and x.life:
+       if a <= 2 and x.life: #and not inplay:
+           #inplay =True
            x.happyness =- a
            print(f"{x.name} randomly got happyness decreased by {a}")
+           if x.happyness <= -5:
+                x.discard()
+                break
+           #inplay =False
 
-       if choice in ["0", "play",  "play", "0: Play"]:
+       if choice in ["0", "play", "0: Play"]:
+           #inplay =True
            y = input("What Game? ")
            x.play(10, y)
            if x.hunger > 20:
                 x.discard()
                 break
+           #inplay =False
        elif choice in ["1", "check stats", "check", "stats","1: Check Stats"]:
            x.check()
        elif choice in ["3", "feed","3: Feed"]:
            z = input("What Food? ")
-           if x.hunger < 0:
+           x.feed(7, z)
+           if x.hunger < -5:
                 x.discard()
                 break
        elif choice in ["2", "discard", "discard pet", "discard of pet","2: Discard Pet"]:
