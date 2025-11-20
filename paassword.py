5e0573d2df566f2f7906b16709e19c9eca93e182
AidenCM1234 <aidenm124@nycstudents.net>
2025-11-20 09:26:31 -0500

    Update paassword.py

--- paassword.py
+++ paassword.py
@@ -1,4 +1,4 @@
-def password(x,y):
+"""def password(x,y):
     strx= False
     if type(x) is str:
         strx= True
@@ -30,9 +30,9 @@ def password(x,y):
             count+=1
             if count >=8:
                 ty =True
-    if ty==False:
-        print(">8 charcters needed")
-        return
+    if len(y)<8:
+        return "Error: >8 charcters needed"
+    
     if ty==True:
         for i in y:
             if i is str.isdigit:
@@ -44,11 +44,36 @@ def password(x,y):
         for i in y:
             if i is str.isupper:
                 ty2 =True
+                print("Valid Password")
     if ty2 == False:
         print("You need at least 1 capatilize")
         return
+    if ty2 == True and tx ==True:
+        print ("ee")
 
 
 
+password("eee@ee","11")"""
 
-password("eee@ee","aa11111aaaaaaaaaaa")
+def create_user(e,p):
+    #e=e.strip()
+    #p=p.strip()
+
+    if not isinstance(e,str) or not isinstance(p,str):
+        print("error:email +password be strings")
+        return
+    if "@" not in e:
+        print("error:email need @")
+        return
+    if len(p)<8:
+        print("error:password >=8 charcters")
+        return
+    if not any(ch.isdigit() for ch in p):
+        print("error password>=1 number")
+        return
+    if not any(ch.isupper() for ch in p):
+        #print("error:password >=1 upper lettter")
+        return"Error:password >=1 upper lettter"
+    print(e,p)
+    
+create_user("@","1eeeeeeeee")
