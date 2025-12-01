c8452bee04a1b22b3bab9792896f91d3fa35133a
AidenCM1234 <aidenm124@nycstudents.net>
2025-12-01 09:26:09 -0500

    Update test.py

---
+++ test.py
@@ -0,0 +1,4 @@
+PS C:\Users\aidenm124\Documents\GitHub\6-pet-project-AidenCM1234> python -m venv appname
+PS C:\Users\aidenm124\Documents\GitHub\6-pet-project-AidenCM1234> cd appname
+PS C:\Users\aidenm124\Documents\GitHub\6-pet-project-AidenCM1234\appname> Scripts\Activate
+(appname) PS C:\Users\aidenm124\Documents\GitHub\6-pet-project-AidenCM1234\appname> pip install requests
